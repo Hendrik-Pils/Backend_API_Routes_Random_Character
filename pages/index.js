2dd2ad5c6904ecc69af8f21827ed5729d42b2d38
Hendrik Pils <hendrik-pils@gmx.de>
2023-12-19 16:29:52 +0100

    finish challenge

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,28 @@
+import useSWR from "swr";
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
 export default function HomePage() {
-  return <h1>Hello from Next.js!</h1>;
+  const { data, isLoading } = useSWR("/api/random-character", fetcher);
+
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+
+  if (!data) {
+    return;
+  }
+  return (
+    <ul>
+      <li>
+        <h1>
+          {data.firstName} {data.lastName}
+        </h1>
+
+        <p>{data.age}</p>
+        <p>{data.twitterName}</p>
+        <p>{data.geoHash}</p>
+      </li>
+    </ul>
+  );
 }
